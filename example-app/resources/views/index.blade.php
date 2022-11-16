859d7888c939c027269445ae5e15b3130e14c66f
s160421001 <s160421001@student.ubaya.ac.id>
2022-11-16 20:09:25 +0700

    update nama

--- example-app/resources/views/index.blade.php
+++ example-app/resources/views/index.blade.php
@@ -9,7 +9,7 @@
 
 </head>
 <body>
-halo nama saya aaron
+halo nama saya steven
 <button type="button" class="btn btn-primary">Primary</button>
 <button type="button" class="btn btn-secondary">Secondary</button>
 <button type="button" class="btn btn-success">Success</button>
